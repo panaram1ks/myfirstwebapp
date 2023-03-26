fd4772b1e5ecf3184960563e3bc6cb9a002665c2
evgen <parom.evgen@mail.ru>
2023-03-26 10:29:27 +0300

    jsp view resolver

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -29,6 +29,14 @@ public class SayHelloController {
         return  sb.toString();
     }
 
-    //JSP
+    //JSP "say-hello-jsp" => sayHello.jsp
+    // /src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+    // /src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
+    // /src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+    // /src/main/resources/META-INF/resources/WEB-INF/jsp/todos.jsp
+    @RequestMapping("say-hello-jsp")
+    public String sayHelloJsp(){
+        return  "sayHello";
+    }
 
 }
