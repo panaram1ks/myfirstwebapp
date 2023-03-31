73c192be7fdfe25bfdd7fc8007e5c9f2debc4e82
evgen <parom.evgen@mail.ru>
2023-03-31 21:32:06 +0300

    add H2 database, tune spring security to show /h2-console

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.in28minutes.springboot.myfirstwebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -37,6 +40,25 @@ public class SpringSecurityConfiguration {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+
+    //All URLs are protected
+    //a login form is shown for unauthorized request
+    //CSRF disable
+    //Frames
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+        http.formLogin(Customizer.withDefaults());
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        return http.build();
+    }
+
 
 
 }
